644436739cab888606ed862a4d8eb974865b7772
Min_h_bot <74346735+K-Minhyeok@users.noreply.github.com>
2024-03-07 18:50:22 +0900

    feat: XSS 공격 / 방어 구현

--- XSSInjection/xss.py
+++ XSSInjection/xss.py
@@ -2,22 +2,6 @@ from flask import *
 app = Flask(__name__)
 app.secret_key = '1234567'
 
-@app.route('/')
-def index():
-    ssti = request.args.get('ssti')
-    html_data = f'''
-    <html>
-    <h1> {ssti} </h1>
-    </html>
-'''
-    return render_template_string(html_data)
-
-@app.route('/b')
-def ssti():
-    ssti = request.args.get('ssti')
-    return render_template('ssti.html',title=ssti)
-
-
 @app.route('/httponly')
 def ho():
     resp=make_response(render_template('xss.html'))
@@ -26,7 +10,7 @@ def ho():
 
     return resp
 
-@app.route('/csp_random')
+@app.route('/csp')
 def csp_test():
     import random
     import string
@@ -34,8 +18,8 @@ def csp_test():
     pool = string.ascii_lowercase
     result = ''
 
-    for i in range(length):
-        result = result + random.choice(pool)
+    for i in range(lenth):
+        result = result + rondom.choice(pool)
 
 
     resp = make_response(render_template('xss.html',nonce = result))
@@ -43,7 +27,7 @@ def csp_test():
     return resp
 
 
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=80,debug=True)
-
 
